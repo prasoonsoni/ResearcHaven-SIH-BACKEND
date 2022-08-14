ff01135542234e6bdefda17aa972aa4b2f88064f
prasoonsoni <prasoonsoni03@gmail.com>
2022-08-14 18:49:04 +0530

    Added Database Connectivity

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const connectToDatabase = require('./database/connection')
 const port = 3000 || process.env.PORT
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+connectToDatabase();
 
 app.get('/', (req, res) => {
     res.send('Welcome to SIH Backend API.')
