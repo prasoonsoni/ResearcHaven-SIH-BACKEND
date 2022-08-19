c4ab5aa103e2006c43aaee7101c70629a755b571
prasoonsoni <prasoonsoni03@gmail.com>
2022-08-19 23:19:47 +0530

    Changed Login Routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post('/create', userController.createUser)
 router.post('/login', userController.loginUser)
 router.get('/verify/:token', userController.verifyUser)
 router.post('/send-verify-mail', userController.sendVerificationEmail)
-router.get('/get-user/', fetchUser, userController.getUser)
-router.get('/get-user/:id', userController.getUserById)
+router.get('/', fetchUser, userController.getUser)
+router.get('/:id', userController.getUserById)
 
 module.exports = router
