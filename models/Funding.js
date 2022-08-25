9dec52712b69e424c4ed38850d3ed2408e632619
prasoonsoni <prasoonsoni03@gmail.com>
2022-08-25 15:54:45 +0530

    Added funding schema

--- models/Funding.js
+++ models/Funding.js
@@ -2,5 +2,12 @@ import mongoose from 'mongoose'
 const { Schema } = mongoose
 
 const FundingSchema = new Schema({
-    
+    user_id: { type: Schema.Types.ObjectId, ref: 'User' },
+    research_proposal_id: { type: Schema.Types.ObjectId, ref: 'ResearchProposal' },
+    amount: { type: Number, default: 0, required: true },
+    duration: { type: Number, default: 0, required: true },
+    organisation_name: { type: String, default: '' },
+    approved_at: { type: Number, default: Date.now() },
 })
+
+export default mongoose.model('Funding', FundingSchema)
